12a17ba5af72253b9d589acadff2f9ef81374f54
Amanda <amanda.lrib@gmail.com>
2025-09-10 11:40:22 -0300

    feature: query products with most revenue

--- 01_sql_bigquery/queries/top_10_products_most_revenue.sql
+++ 01_sql_bigquery/queries/top_10_products_most_revenue.sql
@@ -1,8 +1,21 @@
-SELECT order_items.product_id, products.name, products.category, COUNT(*) AS total_orders
-FROM `bigquery-public-data.thelook_ecommerce.products` as products 
-JOIN `bigquery-public-data.thelook_ecommerce.order_items` as order_items
-ON products.id = order_items.product_id
-GROUP BY order_items.product_id, products.name, products.category
-ORDER BY total_orders DESC
-LIMIT 10
-
+SELECT
+  order_items.product_id,
+  products.name,
+  products.category,
+  SUM(order_items.sale_price) AS total_revenue
+FROM
+  `bigquery-public-data.thelook_ecommerce.products` AS products
+JOIN
+  `bigquery-public-data.thelook_ecommerce.order_items` AS order_items
+ON
+  products.id = order_items.product_id
+WHERE
+  order_items.status = 'Complete'
+GROUP BY
+  order_items.product_id,
+  products.name,
+  products.category
+ORDER BY
+  total_revenue DESC
+LIMIT
+  10
